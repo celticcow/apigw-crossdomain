d45acadd8684f5e1da7282b297ed66c60a16cecd
Celticcow <gregory.dunlap@gmail.com>
2020-11-16 13:29:22 -0600

    basic client working ... json sanity from server not so much

--- client-test.py
+++ client-test.py
@@ -1,5 +1,41 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python3 -W ignore::DeprecationWarning
 
 import json
-import request
+import requests
+
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+"""
+Greg_Dunlap / CelticCow
+
+test script to do json query to api gw
+
+curl http://localhost:5000/crossdomain -X POST --data '{"ipaddr" : "146.18.2.137"}'
+"""
+
+
+def main():
+    print("in main")
+
+    ip_json = {
+        "ipaddr" : "146.18.2.13"
+    }
+
+    url = "http://localhost:5000/crossdomain"
+    
+    r = requests.post(url, json.dumps(ip_json))
+
+    data = r.json()
+
+    print("-------------------------------------------------")
+    print(data)
+
+    #for x in range(data['total']):
+        #print(data[cmas][x])
+    
+
+if __name__ == "__main__":
+    main()
+#end of program
