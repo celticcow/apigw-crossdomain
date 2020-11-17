1bc01e2dd8e6e2327cad7f8384fb83548702b2ca
Celticcow <gregory.dunlap@gmail.com>
2020-11-17 13:30:50 -0600

    client testing

--- client-test.py
+++ client-test.py
@@ -20,7 +20,7 @@ def main():
     print("in main")
 
     ip_json = {
-        "ipaddr" : "146.18.2.137"
+        "ipaddr" : "146.18.4.144"
     }
 
     url = "http://localhost:5000/crossdomain"
@@ -35,8 +35,39 @@ def main():
     print(data['total'])
 
     for x in range(data['total']):
+        print("*-" *20)
+
+        print("CMA ", end="\t")
         print(data['cmas'][x]['cma'])
-        print(data['cmas'][x]['whereused'])
+        
+        print("Where-Used ", end="")
+
+        if(len(data['cmas'][x]['whereused']['cpobj']) == 0):
+            print("not used\n")
+        else:
+            #print(data['cmas'][x]['whereused']) 
+            tmp1 = data['cmas'][x]['whereused']['cpobj'][0]['access-control-rules']
+            for i in range(len(tmp1)):
+                print("rule")
+                print(data['cmas'][x]['whereused']['cpobj'][0]['access-control-rules'][i])
+                print("\n--\n")
+
+            tmp2 = data['cmas'][x]['whereused']['cpobj'][0]['objects']
+            for j in range(len(tmp2)):
+                print("object")
+                print(data['cmas'][x]['whereused']['cpobj'][0]['objects'][j])
+                print("\n^^\n")
+            #print(data['cmas'][x]['whereused']['cpobj'])
+            #print("\n________________\n")
+            #print(tmp1)
+            #print("\n")
+            #print(tmp2)
+            print("\n**************\n")
+            #print(data['cmas'][x]['whereused']['cpobj']['objects'])
+
+        #tmp = data['cmas'][x]['whereused']
+
+        print(len(data['cmas'][x]['whereused']['cpobj']))
     
 
 if __name__ == "__main__":
